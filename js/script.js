c97392f35e84186956375488b36632c32faba806
Krystian Kwiatos <matius4321@gmail.com>
2021-10-08 14:33:26 +0200

    zadanie blog

---
+++ js/script.js
@@ -0,0 +1,48 @@
+/*
+document.getElementById('test-button').addEventListener('click', function(){
+    const links = document.querySelectorAll('.titles a');
+    console.log('links:', links);
+  });
+*/
+const titleClickHandler = function(event){
+    const clickedElement = this;
+    event.preventDefault();
+    console.log('Link was clicked!')
+    console.log(event);
+  
+    /* remove class 'active' from all article links  */
+    
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+    }
+  
+    /* add class 'active' to the clicked link */
+    clickedElement.classList.add('active');
+  
+    /* remove class 'active' from all articles */
+
+    const activeArticles = document.querySelectorAll('.posts p.active');
+
+    for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+    }
+  
+    /* get 'href' attribute from the clicked link */
+
+    const articleSelector = clickedElement.getAttribute('href');
+  
+    /* find the correct article using the selector (value of 'href' attribute) */
+  
+    /* add class 'active' to the correct article */
+
+    const clickedArticle = document.querySelector(articleSelector);
+    clickedArticle.classList.add('active');
+    };
+  
+  const links = document.querySelectorAll('.titles a');
+  
+  for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+  }
